27420111a5f684819180840bf02919ca418f99b6
Analise <analise.n.medina@gmail.com>
2024-06-17 23:26:17 -0700

    acheived going from Ensembl to NCBI accession codes with one homo sapien gene at a time

--- Ensembl_to_NCBI_AM_061324.py
+++ Ensembl_to_NCBI_AM_061324.py
@@ -1,4 +1,4 @@
-#Ensembl to GenBank + RefSeq for NCBI. 
+#Ensembl to NCBI Accession Codes for homo sapiens. 
 #This is just my first attempt at this kind of project and am only aiming to print out the ID's for NCBI. 
 #Example: ENSG00000012048 
 import requests
@@ -6,48 +6,58 @@ import re
 from Bio import Entrez
 import urllib.parse
 import json
+import xml.etree.ElementTree as ET
 
 Entrez.email = input("In compliance with NCBI policy, please input your email:") 
-e_id = input("Please type in a valid Ensembl ID:") 
+e_id = input("Please type in a valid Ensembl ID:").strip() 
 #For future scripts, will try to have this be a list or data frame column to loop through 
 # but let's just focus on getting this to work with one for now. - 061324
 
-#search Ensembl API for 'e_id' and obtain the gene name - function
 def database_search(gene_id):
+    
     url = f"https://rest.ensembl.org/lookup/id/{gene_id}?content-type=application/json"
+    summary_url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi" 
     response = requests.get(url)
-    genbank_ids=[]
-    refseq_ids = []
+    
     if response.status_code==200:
         data = response.json()
+        #print(f"Ensembl data: {data}") This is only for current troubleshooting and will be taken out later 
+        
         display_name = data.get("display_name", "No display name found.")
-        print("The gene's name is " + display_name)
-        url = urllib.parse.quote(display_name)
-        search_handle = Entrez.esearch(db="gene", term=url,remode="json")
-        search_results = Entrez.read(search_handle)
-        search_handle.close()
-        gene_ids = search_results["IdList"]
+        print(f"Gene name is {display_name}")
+        search_term = f"{display_name}[Gene] AND Homo sapiens[Organism]"
+        
+        handle = Entrez.esearch(db="gene", term=search_term)
+        record = Entrez.read(handle)
+        handle.close()
+        
+        if record["IdList"]:
+            
+            gene_id = record["IdList"][0]
+            print(f"NCBI Gene ID:{gene_id}")
+            
+            summary_handle = Entrez.esummary(db="gene", id=gene_id)
+            summary_record = Entrez.read(summary_handle)
+            summary_handle.close()
+        
+            gene_info = summary_record["DocumentSummarySet"]["DocumentSummary"][0]
+            #print(f"NCBI Gene Info: {gene_info}")
+            #Works up to this point - 061724
+            accession_codes = [i['ChrAccVer'] for i in gene_info.get("GenomicInfo", []) if "ChrAccVer" in i]
+            print(f"NCBI Accession Codes: {accession_codes}")
+        else:
+            print(f"No accession code was found for {display_name} in NCBI")
+            
+
+        
+        
+
+        
+
     else:
         print(f"Failed to retrieve data: {response.status_code}")
-    for i in gene_ids:
-        fetch_handle = Entrez.efetch(db="gene", id=i, retmode="json")
-        #bug to be fixed here, error = Exception has occurred: JSONDecodeError - 061324
-        gene_info = json.load(fetch_handle)
-        fetch_handle.close()
-        refseq_id = gene_info.get("refseqrna",[])
-        genbank_id=gene_info.get("genbank", [])
-        refseq_ids.append(refseq_id)
-        genbank_ids.append(genbank_id)
-    print(refseq_ids)
-    print(genbank_ids)
-        
-    
-        
-        
-    
-#use obtained gene name to search the NCBI API and obtain the GeneBank and RefSeq ID's - function
+    print(accession_codes)
 
-#Save those codes and print them out. 
 
 #Actual Loop
 #Check if ID is good. "ENSG" for humans otherwise, 
@@ -68,4 +78,7 @@ if re.match(e_id_format, e_id) or re.match(e_id_format2, e_id):
     #Will probably have the ID's saved in a list in case there are isotopes or other versions of hte gene that need to be printed out. 
 else:
     print("Invalid ID")
+    
+
+#Still need to add error handling and figure out what format we want the output to be now that I am able to produce a result. - 061724
     
