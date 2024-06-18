1cd33530969c35d628dd09de9c29acc716397d30
John Paul Wick <johnpaulwick@gmail.com>
2024-06-18 17:06:31 -0400

    Converter function going from MyGene ID to OMIM ID

---
+++ Convert_MyGene_to_OMIM.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# MyGene ID to OMIM ID converter function
+
+# Modules required for this function:
+import requests
+
+
+def Convert_MyGene_to_OMIM(mygeneid):
+    '''This function takes a MyGene ID number as an input, and prints the OMIM ID number as an output.
+    If there is not a match in the MyGene database, it prints an error message that the ID is not valid.'''
+    r = requests.get(f"https://mygene.info/v3/gene/{mygeneid}?fields=all&dotfield=false&size=10")
+    r = r.json()
+    try: 
+        OMIM_ID = r["MIM"]
+        print(OMIM_ID)
+    except:
+        print("Error: Invalid MyGene ID")
+
+
+# Testing the function:
+Convert_MyGene_to_OMIM("MC1R")
