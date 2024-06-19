d4881bb7f88fa0bc2fa830556711d0bb98929422
John Paul Wick <johnpaulwick@gmail.com>
2024-06-19 14:35:49 -0400

    Changed printing to logging for error messages. Now have three functions: symbol to MyGene ID, MyGene ID to OMIM ID, and symbol to OMIM ID. I am not sure how to implement the OMIM API into this as it requires a unique key for each user for every search. So I'm not sure how to get around that to make a function which goes from OMIM ID to MyGene ID, or from gene symbol to OMIM ID directly without going through MyGene which might be cleaner. Any ideas appreciated!

--- Convert_MyGene_to_OMIM.py
+++ Convert_MyGene_to_OMIM.py
@@ -1,23 +1,50 @@
 #!/usr/bin/env python3
 
-# MyGene ID to OMIM ID converter function
+# Gene symbol to MyGene ID function, MyGene ID to OMIM ID converter function, and Gene symbol to OMIM ID function
 
-# Modules required for this function:
+# Modules required:
 import requests
+import logging
+
+# Function to retrieve MyGene ID from Gene common symbol:
+def get_MyGene(gene):
+    '''This function takes a gene symbol as an input, and returns the MyGene ID number for the gene.
+    If there is not a match in the MyGene database, an error message is logged that the symbol is not valid.'''
+    r = requests.get(f"https://mygene.info/v3/query?q={gene}&fields=all&size=10&from=0&fetch_all=false&facet_size=10&entrezonly=false&ensemblonly=false&dotfield=false")
+    r = r.json()
+    try: 
+        MyGene_ID = r["hits"][0]["_id"]
+        return MyGene_ID
+    except:
+        logging.error("Error: Invalid Gene Symbol")
+        return None
 
 
-def Convert_MyGene_to_OMIM(mygeneid):
+
+def convert_MyGene_to_OMIM(mygeneid):
     '''This function takes a MyGene ID number as an input, and prints the OMIM ID number as an output.
-    If there is not a match in the MyGene database, it prints an error message that the ID is not valid.
+    If there is not a match in the MyGene database, an error message is logged that the ID is not valid.
     Note: This function is not meant for obtaining the OMIM ID from the common gene code name, only for converting from the MyGene ID number.'''
     r = requests.get(f"https://mygene.info/v3/gene/{mygeneid}?fields=all&dotfield=false&size=10")
     r = r.json()
     try: 
         OMIM_ID = r["MIM"]
-        print(OMIM_ID)
+        return OMIM_ID
     except:
-        print("Error: Invalid MyGene ID")
+        logging.error("Error: Invalid MyGene ID or Gene Not Found in OMIM Database")
+        return None
 
 
-# Testing the function:
-Convert_MyGene_to_OMIM(1000)
+def get_OMIM(gene):
+    '''This function takes a gene symbol as an input, and returns the OMIM ID number for the gene.
+    This function uses the previous two functions, so an appropriate error message is logged at any step that fails.'''
+    mygeneid = get_MyGene(gene)
+    omimid = convert_MyGene_to_OMIM(mygeneid)
+    return omimid
+
+
+  
+# Testing the functions:
+get_MyGene("MC1R")       
+convert_MyGene_to_OMIM(4157)
+get_OMIM("MC1R")
