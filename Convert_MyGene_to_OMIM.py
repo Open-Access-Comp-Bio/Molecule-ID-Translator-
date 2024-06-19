f65f5c55a0df74fd00ec1ffe0760d48843b7bd15
John Paul Wick <johnpaulwick@gmail.com>
2024-06-19 19:35:31 -0400

    function description updated

--- Convert_MyGene_to_OMIM.py
+++ Convert_MyGene_to_OMIM.py
@@ -24,7 +24,7 @@ def get_MyGene(gene):
 def convert_MyGene_to_OMIM(mygeneid):
     '''This function takes a MyGene ID number as an input, and prints the OMIM ID number as an output.
     If there is not a match in the MyGene database, an error message is logged that the ID is not valid.
-    Note: This function is not meant for obtaining the OMIM ID from the common gene code name, only for converting from the MyGene ID number.'''
+    Note: This function is not meant for obtaining the OMIM ID from the common gene symbol, only for converting from the MyGene ID number.'''
     r = requests.get(f"https://mygene.info/v3/gene/{mygeneid}?fields=all&dotfield=false&size=10")
     r = r.json()
     try: 
